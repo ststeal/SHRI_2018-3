baacbd124677564c993a86341e4c2352fa78efcc
ststeal <ststealnik@yandex.ru>
2018-08-30 22:04:50 +0300

    2nd step

--- main.js
+++ main.js
@@ -3,8 +3,9 @@ function arrayPush(array, value1, value2, value3, value4) {
         "hour": value1,
         "value": value2,
         "mode": value3,
+        "currentPower": 0,
+        "devices": [],
         "maxPower": value4,
-        "devices": []
     });
 }
 
@@ -24,10 +25,9 @@ function separateRates(data) {
             }
         }
     });
-    // return separatedArr.sort(function (a, b) {
-    //     return a.hour - b.hour;
-    // });
-    return separatedArr;
+    return separatedArr.sort(function (a, b) {
+        return a.hour - b.hour;
+    });
 }
 
 function enchantDevices(data) {
@@ -39,17 +39,133 @@ function enchantDevices(data) {
             "power": item.power,
             "duration": item.duration,
             "mode": item.mode,
-            "powdur": item.duration * item.power
+            "powdur": item.duration * item.power,
         });
+    });
+    devices.sort(function (a, b) {
+        if (b.duration === 24) {
+            return b.duration - a.duration;
+        }
+        if (a.duration === 24) {
+            return a.duration - b.duration;
+        }
+        if (!!b.mode === !!a.mode) {
+            if (b.powdur === a.powdur) {
+                return b.power - a.power;
+            } else {
+                return b.powdur - a.powdur;
+            }
+        } else {
+            return !a.mode - !b.mode;
+        }
     });
     return devices;
 }
 
+function computingShedule(timeLine, devices) {
+    let fullValue = 0;
+    devices.forEach(function (device) {
+
+        let bestHour;
+        let deviceFullValue = 0;
+        let bestDeviceFullValue;
+
+        if (device.duration === 24) {
+            timeLine.forEach(function (hour) {
+
+                hour.devices.push(device.id);
+                hour.currentPower += device.power;
+                deviceFullValue += device.power * hour.value;
+                device.duration--;
+                // if (hour.currentPower > hour.maxPower) {
+                //     console.log('Error');
+                // }
+
+            });
+            fullValue += deviceFullValue;
+
+        } else {
+            if (device.mode) {
+                timeLine.forEach(function (hour, index) {
+
+                    if (hour.mode === device.mode) {
+                        let counter = 0;
+                        if (hour.currentPower + device.power <= hour.maxPower) {
+                            for (let i = 0; i < device.duration; i++) {
+                                let currentHour = (index + i) % timeLine.length;
+                                if (timeLine[currentHour].mode === device.mode) {
+                                    deviceFullValue += device.power * timeLine[currentHour].value;
+                                    counter++;
+                                }
+                            }
+                            if (counter === device.duration) {
+                                if (!bestDeviceFullValue) {
+                                    bestDeviceFullValue = deviceFullValue;
+                                    bestHour = hour.hour;
+                                    deviceFullValue = 0;
+                                } else {
+                                    if (deviceFullValue < bestDeviceFullValue) {
+                                        bestDeviceFullValue = deviceFullValue;
+                                        bestHour = hour.hour;
+                                        deviceFullValue = 0;
+                                    } else {
+                                        deviceFullValue = 0;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                });
+
+            } else {
+                timeLine.forEach(function (hour, index) {
+
+                    if (hour.currentPower + device.power <= hour.maxPower) {
+                        for (let i = 0; i < device.duration; i++) {
+                            deviceFullValue += device.power * timeLine[(index + i) % timeLine.length].value;
+                        }
+                        if (!bestDeviceFullValue) {
+                            bestDeviceFullValue = deviceFullValue;
+                            bestHour = hour.hour;
+                            deviceFullValue = 0;
+                        } else {
+                            if (deviceFullValue < bestDeviceFullValue) {
+                                bestDeviceFullValue = deviceFullValue;
+                                bestHour = hour.hour;
+                                deviceFullValue = 0;
+                            } else {
+                                deviceFullValue = 0;
+                            }
+                        }
+                    }
+
+                });
+            }
+            if (bestHour>=0){
+                for (let i = 0; i < device.duration; i++) {
+                    console.log(device);
+                    let currentHour = (bestHour + i) % timeLine.length;
+                    timeLine[currentHour].devices.push(device.id);
+                    timeLine[currentHour].currentPower += device.power;
+                }
+                fullValue += bestDeviceFullValue;
+            }else{
+                console.log(device.name+' '+device.id+' : не может быть размещено')
+            }
+        }
+
+    });
+    console.log(fullValue);
+}
+
 function smart(data) {
     let timeLine = separateRates(data);
-    let deviceArr = enchantDevices(data);
+    let devicesArr = enchantDevices(data);
+    console.log(devicesArr);
     console.log(timeLine);
-    console.log(deviceArr);
+    computingShedule(timeLine, devicesArr);
+    console.log(timeLine);
 }
 
 const data1 = {
@@ -114,7 +230,202 @@ const data1 = {
             "value": 1.79
         }
     ],
+    "maxPower": 2099
+};
+const data2 = {
+    "devices": [
+        {
+            "id": "1",
+            "name": "Посудомоечная машина",
+            "power": 950,
+            "duration": 3,
+            "mode": "night"
+        },
+        {
+            "id": "2",
+            "name": "Духовка",
+            "power": 1200,
+            "duration": 5,
+            "mode": "day"
+        },
+        {
+            "id": "3",
+            "name": "Духовка2",
+            "power": 500,
+            "duration": 10,
+            "mode": "day"
+        },
+        {
+            "id": "4",
+            "name": "Холодильник",
+            "power": 50,
+            "duration": 24
+        },
+        {
+            "id": "5",
+            "name": "Термостат",
+            "power": 50,
+            "duration": 24
+        },
+        {
+            "id": "6",
+            "name": "Термостат2",
+            "power": 100,
+            "duration": 24
+        },
+        {
+            "id": "7",
+            "name": "Кондиционер",
+            "power": 850,
+            "duration": 1
+        },
+        {
+            "id": "8",
+            "name": "Кондиционер",
+            "power": 300,
+            "duration": 1,
+            "mode": "day"
+        },
+        {
+            "id": "9",
+            "name": "Кондиционер",
+            "power": 850,
+            "duration": 1
+        }
+    ],
+    "rates": [
+        {
+            "from": 7,
+            "to": 10,
+            "value": 6.46
+        },
+        {
+            "from": 10,
+            "to": 17,
+            "value": 5.38
+        },
+        {
+            "from": 17,
+            "to": 21,
+            "value": 6.46
+        },
+        {
+            "from": 21,
+            "to": 23,
+            "value": 5.38
+        },
+        {
+            "from": 23,
+            "to": 7,
+            "value": 1.79
+        }
+    ],
     "maxPower": 2100
+};
+const data3 = {
+    "devices": [
+        // {
+        //     "id": "1",
+        //     "name": "Посудомоечная машина",
+        //     "power": 950,
+        //     "duration": 3,
+        //     "mode": "night"
+        // },
+        // {
+        //     "id": "2",
+        //     "name": "Духовка",
+        //     "power": 2000,
+        //     "duration": 2,
+        //     "mode": "day"
+        // },
+        {
+            "id": "3",
+            "name": "Духовка2",
+            "power": 2000,
+            "duration": 4,
+            "mode": "night"
+        },
+        {
+            "id": "4",
+            "name": "Духовка3",
+            "power": 2000,
+            "duration": 4,
+            "mode": "night"
+        }
+    ],
+    "rates": [
+        {
+            "from": 7,
+            "to": 10,
+            "value": 6.46
+        },
+        {
+            "from": 10,
+            "to": 17,
+            "value": 5.38
+        },
+        {
+            "from": 17,
+            "to": 21,
+            "value": 6.46
+        },
+        {
+            "from": 21,
+            "to": 23,
+            "value": 5.38
+        },
+        {
+            "from": 2,
+            "to": 7,
+            "value": 2
+        },
+        {
+            "from": 23,
+            "to": 2,
+            "value": 1.5
+        }
+    ],
+    "maxPower": 2100
+};
+const data4 = {
+    "devices": [
+
+        {
+            "id": "7D9DC84AD110500D284B33C82FE6E85E",
+            "name": "Кондиционер",
+            "power": 850,
+            "duration": 11,
+            "mode": "night"
+        }
+    ],
+    "rates": [
+        {
+            "from": 7,
+            "to": 10,
+            "value": 6
+        },
+        {
+            "from": 10,
+            "to": 17,
+            "value": 1
+        },
+        {
+            "from": 17,
+            "to": 21,
+            "value": 6
+        },
+        {
+            "from": 21,
+            "to": 23,
+            "value": 5
+        },
+        {
+            "from": 23,
+            "to": 7,
+            "value": 2
+        }
+    ],
+    "maxPower": 2099
 };
 
 const outData = {
