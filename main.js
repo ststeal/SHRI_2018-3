35b5ecf705dfc6227fa967216e92eef770ae8809
ststeal <ststealnik@yandex.ru>
2018-09-03 12:52:46 +0300

    4th step

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
+/*
+* Функция делает из интервалов тарифов один массив разбитый по часам с
+* currentPower, devices, hour, maxPower, mode, value
+* */
 function separateRates(data) {
     let separatedArr = [];
     data.rates.forEach(function (item) {
         if (item.to < item.from) {
             for (let i = item.from; i <= 23; i++) {
-                ratePush(((7 <= i) && (i < 21)) ? 'day' : 'night', i,item);
+                ratePush(((7 <= i) && (i < 21)) ? 'day' : 'night', i, item);
             }
             for (let i = 0; i < item.to; i++) {
-                ratePush(((7 <= i) && (i < 21)) ? 'day' : 'night', i,item);
+                ratePush(((7 <= i) && (i < 21)) ? 'day' : 'night', i, item);
             }
         } else {
             for (let i = item.from; i < item.to; i++) {
-                ratePush(((7 <= i) && (i < 21)) ? 'day' : 'night', i,item);
+                ratePush(((7 <= i) && (i < 21)) ? 'day' : 'night', i, item);
             }
         }
     });
@@ -18,7 +22,7 @@ function separateRates(data) {
         return a.hour - b.hour;
     });
 
-    function ratePush(mode, index,item) {
+    function ratePush(mode, index, item) {
         separatedArr.push({
             'hour': index,
             'value': item.value,
@@ -30,6 +34,9 @@ function separateRates(data) {
     }
 }
 
+/*
+* Функция сортирует девайсы по приоритету расстановки
+ */
 function enchantDevices(data) {
     let devices = [];
     data.devices.forEach(function (item) {
@@ -62,122 +69,112 @@ function enchantDevices(data) {
     return devices;
 }
 
-function computingShedule(timeLine, devices) {
+/*
+* Функция расставляет каждый девайс на промежуток с максимально
+* эффективным энергопотреблением
+ */
+function computingSchedule(timeLine, devices) {
     let outputData = {
-        'shedule': {},
-        'consumedEnergy': {
-            'devices': {}
-        }
+        'schedule': {},
+        'consumedEnergy':
+            {
+                'devices': {}
+            }
     };
     let fullValue = 0;
+
     devices.forEach(function (device) {
 
         let bestHour;
         let deviceFullValue = 0;
         let bestDeviceFullValue = 0;
 
-        if (device.duration === 24) {
-            timeLine.forEach(function (hour) {
+        function compareResult(hour) {
+            if (!bestDeviceFullValue) {
+                bestDeviceFullValue = deviceFullValue;
+                bestHour = hour.hour;
+                deviceFullValue = 0;
+            } else {
+                if (deviceFullValue < bestDeviceFullValue) {
+                    bestDeviceFullValue = deviceFullValue;
+                    bestHour = hour.hour;
+                    deviceFullValue = 0;
+                } else {
+                    deviceFullValue = 0;
+                }
+            }
+        }
 
+        if (device.duration === 24) {
+
+            timeLine.forEach(function (hour) {
                 hour.devices.push(device.id);
                 hour.currentPower += device.power;
                 bestDeviceFullValue += device.power * hour.value;
                 device.duration--;
-                // if (hour.currentPower > hour.maxPower) {
-                //     console.log('Error');
-                // }
-
             });
 
         } else {
-            bestDeviceFullValue = 0;
-            if (device.mode) {
-                timeLine.forEach(function (hour, index) {
 
-                    if (hour.mode === device.mode) {
-                        let counter = 0;
-                        if (hour.currentPower + device.power <= hour.maxPower) {
-                            for (let i = 0; i < device.duration; i++) {
-                                let currentHour = (index + i) % timeLine.length;
-                                if (timeLine[currentHour].mode === device.mode) {
+            timeLine.forEach(function (hour, index) {
+                    let counter = 0;
+                    if ((hour.currentPower + device.power) <= hour.maxPower) {
+                        for (let i = 0; i < device.duration; i++) {
+                            let currentHour = (index + i) % timeLine.length;
+                            if (device.mode) {
+                                if ((timeLine[currentHour].currentPower + device.power <= timeLine[currentHour].maxPower) &&
+                                    (timeLine[currentHour].mode === device.mode)) {
+                                    deviceFullValue += device.power * timeLine[currentHour].value;
+                                    counter++;
+                                }
+                            } else {
+                                if (timeLine[currentHour].currentPower + device.power <= timeLine[currentHour].maxPower) {
                                     deviceFullValue += device.power * timeLine[currentHour].value;
                                     counter++;
                                 }
                             }
-                            if (counter === device.duration) {
-                                if (!bestDeviceFullValue) {
-                                    bestDeviceFullValue = deviceFullValue;
-                                    bestHour = hour.hour;
-                                    deviceFullValue = 0;
-                                } else {
-                                    if (deviceFullValue < bestDeviceFullValue) {
-                                        bestDeviceFullValue = deviceFullValue;
-                                        bestHour = hour.hour;
-                                        deviceFullValue = 0;
-                                    } else {
-                                        deviceFullValue = 0;
-                                    }
-                                }
-                            }
                         }
                     }
-
-                });
-
-            } else {
-                timeLine.forEach(function (hour, index) {
-
-                    if (hour.currentPower + device.power <= hour.maxPower) {
-                        for (let i = 0; i < device.duration; i++) {
-                            deviceFullValue += device.power * timeLine[(index + i) % timeLine.length].value;
-                        }
-                        if (!bestDeviceFullValue) {
-                            bestDeviceFullValue = deviceFullValue;
-                            bestHour = hour.hour;
-                            deviceFullValue = 0;
-                        } else {
-                            if (deviceFullValue < bestDeviceFullValue) {
-                                bestDeviceFullValue = deviceFullValue;
-                                bestHour = hour.hour;
-                                deviceFullValue = 0;
-                            } else {
-                                deviceFullValue = 0;
-                            }
-                        }
+                    if (counter === device.duration) {
+                        compareResult(hour);
                     }
+                }
+            );
 
-                });
-            }
-            if (bestHour >= 0) {
+            if (bestHour !== undefined) {
                 for (let i = 0; i < device.duration; i++) {
                     let currentHour = (bestHour + i) % timeLine.length;
                     timeLine[currentHour].devices.push(device.id);
                     timeLine[currentHour].currentPower += device.power;
                 }
             } else {
-                console.log(device.name + ' ' + device.id + ' : не может быть размещено')
+                console.log(device.name + ' ' + device.id + ' : can\'t be placed')
             }
         }
+
         fullValue += bestDeviceFullValue;
         outputData.consumedEnergy.devices[device.id] = bestDeviceFullValue * 0.001;
-    });
+    })
+    ;
     timeLine.forEach(function (hour, index) {
-        outputData.shedule[index] = [];
+        outputData.schedule[index] = [];
         hour.devices.forEach(function (device) {
-            outputData.shedule[index].push(device)
+            outputData.schedule[index].push(device)
         });
     });
     outputData.consumedEnergy.value = fullValue * 0.001;
     return outputData;
 }
 
-
-function smart(data) {
+/*
+* Результирующая функция
+ */
+function smartHouse(data) {
     let timeLine = separateRates(data);
     let devicesArr = enchantDevices(data);
     console.log(devicesArr);
     console.log(timeLine);
-    let output = computingShedule(timeLine, devicesArr);
+    let output = computingSchedule(timeLine, devicesArr);
     console.log(timeLine);
     return output;
 }
@@ -249,62 +246,43 @@ const data1 = {
 const data2 = {
     'devices': [
         {
-            'id': '1',
+            'id': 'washup',
             'name': 'Посудомоечная машина',
             'power': 950,
             'duration': 3,
             'mode': 'night'
         },
         {
-            'id': '2',
+            'id': 'duhovka',
             'name': 'Духовка',
-            'power': 1200,
-            'duration': 5,
+            'power': 1500,
+            'duration': 2,
             'mode': 'day'
         },
         {
-            'id': '3',
-            'name': 'Духовка2',
-            'power': 500,
-            'duration': 10,
-            'mode': 'day'
-        },
-        {
-            'id': '4',
+            'id': 'refrejerator',
             'name': 'Холодильник',
             'power': 50,
-            'duration': 24
+            'duration': 23
         },
         {
-            'id': '5',
+            'id': 'termostat',
             'name': 'Термостат',
             'power': 50,
-            'duration': 24
+            'duration': 22
         },
         {
-            'id': '6',
-            'name': 'Термостат2',
-            'power': 100,
-            'duration': 24
-        },
-        {
-            'id': '7',
+            'id': 'kondei',
             'name': 'Кондиционер',
             'power': 850,
             'duration': 1
         },
         {
-            'id': '8',
-            'name': 'Кондиционер',
-            'power': 300,
-            'duration': 1,
+            'id': 'paialnik',
+            'name': 'Паяльник',
+            'power': 1500,
+            'duration': 7,
             'mode': 'day'
-        },
-        {
-            'id': '9',
-            'name': 'Кондиционер',
-            'power': 850,
-            'duration': 1
         }
     ],
     'rates': [
@@ -334,7 +312,7 @@ const data2 = {
             'value': 1.79
         }
     ],
-    'maxPower': 2100
+    'maxPower': 2099
 };
 const data3 = {
     'devices': [
@@ -440,43 +418,4 @@ const data4 = {
         }
     ],
     'maxPower': 2099
-};
-
-const outData = {
-    'schedule': {
-        '0': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E', 'F972B82BA56A70CC579945773B6866FB'],
-        '1': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E', 'F972B82BA56A70CC579945773B6866FB'],
-        '2': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E', 'F972B82BA56A70CC579945773B6866FB'],
-        '3': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E'],
-        '4': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E'],
-        '5': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E'],
-        '6': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E'],
-        '7': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E'],
-        '8': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E'],
-        '9': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E'],
-        '10': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E', 'C515D887EDBBE669B2FDAC62F571E9E9'],
-        '11': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E', 'C515D887EDBBE669B2FDAC62F571E9E9'],
-        '12': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E'],
-        '13': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E'],
-        '14': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E'],
-        '15': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E'],
-        '16': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E'],
-        '17': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E'],
-        '18': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E'],
-        '19': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E'],
-        '20': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E'],
-        '21': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E'],
-        '22': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E'],
-        '23': ['02DDD23A85DADDD71198305330CC386D', '1E6276CC231716FE8EE8BC908486D41E', '7D9DC84AD110500D284B33C82FE6E85E']
-    },
-    'consumedEnergy': {
-        'value': 38.939,
-        'devices': {
-            'F972B82BA56A70CC579945773B6866FB': 5.1015,
-            'C515D887EDBBE669B2FDAC62F571E9E9': 21.52,
-            '02DDD23A85DADDD71198305330CC386D': 5.398,
-            '1E6276CC231716FE8EE8BC908486D41E': 5.398,
-            '7D9DC84AD110500D284B33C82FE6E85E': 1.5215
-        }
-    }
 };
